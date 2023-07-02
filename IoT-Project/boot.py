34a250f6c92ea8f3c37d8dd7e8f7475948ad1186
Sven-Lindstroem <117860880+Sven-Lindstroem@users.noreply.github.com>
2023-07-02 12:23:29 +0200

    Update boot.py

--- IoT-Project/boot.py
+++ IoT-Project/boot.py
@@ -2,24 +2,29 @@ def do_connect():
     import network
     from time import sleep
     from config import wifi_secrets
-    import machine
 
+# put the modem on station mode
+    wlan = network.WLAN(network.STA_IF)
 
-    wlan = network.WLAN(network.STA_IF)     # put modem on station mode
-
-    if not wlan.isconnected():              # checl if already connected          
+ # checl if already connected
+    if not wlan.isconnected():          
         print("connecting to network...")
-        wlan.active(True)                   # activate network interface
-        # set power mode to get WIFI power-saving off (if needed)
-        wlan.config(pm = 0xa11140)
-        wlan.connect(wifi_secrets["ssid"], wifi_secrets["password"]) # Your WiFi credential
-        
+
+# activate network interface
+        wlan.active(True)
+        wlan.config(pm="PM_POWERSAVE")
+
+# Your WiFi credential
+        wlan.connect(wifi_secrets["ssid"], wifi_secrets["password"])
+
         print("waiting for connection...", end='')
-        # checl if it is connected otherwise wait
+
+# checl if it is connected otherwise wait
         while not wlan.isconnected() and wlan.status() >= 0:
             print('.',end='')
             sleep(1)
-    # pritn the IP assigned by router
+
+# pritn the IP assigned by router
     ip = wlan.ifconfig()[0]
     print('\nConnected on {}'.format(ip))
     return ip
@@ -28,5 +33,3 @@ try:
     ip = do_connect()
 except KeyboardInterrupt:
     print("Keyboard interrupt")
-
-
