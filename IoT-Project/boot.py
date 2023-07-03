0cd5e3f17419ef7e9d7c778f40cd87eb3e026935
Sven-Lindstroem <117860880+Sven-Lindstroem@users.noreply.github.com>
2023-07-03 13:48:43 +0200

    Update boot.py

--- IoT-Project/boot.py
+++ IoT-Project/boot.py
@@ -3,28 +3,27 @@ def do_connect():
     from time import sleep
     from config import wifi_secrets
 
-# put the modem on station mode
+    # put the modem on station mode
     wlan = network.WLAN(network.STA_IF)
 
- # checl if already connected
+    # checl if already connected
     if not wlan.isconnected():          
         print("connecting to network...")
 
-# activate network interface
+        # activate network interface
         wlan.active(True)
-        wlan.config(pm="PM_POWERSAVE")
 
-# Your WiFi credential
+        # Your WiFi credential
         wlan.connect(wifi_secrets["ssid"], wifi_secrets["password"])
 
         print("waiting for connection...", end='')
 
-# checl if it is connected otherwise wait
+        # checl if it is connected otherwise wait
         while not wlan.isconnected() and wlan.status() >= 0:
             print('.',end='')
             sleep(1)
 
-# pritn the IP assigned by router
+    # pritn the IP assigned by router
     ip = wlan.ifconfig()[0]
     print('\nConnected on {}'.format(ip))
     return ip
